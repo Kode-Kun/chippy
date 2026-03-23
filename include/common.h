1be1363cc4686b66a74e19f3ae0a19c8e0505f41
Kode-Kun <luisantcard@gmail.com>
2026-03-23 18:49:15 -0400

    added token data types

--- include/common.h
+++ include/common.h
@@ -7,7 +7,47 @@
 
 #define MEM_SIZE          4096
 
-union opcode{
+typedef enum {
+  TokenMov,      // 8XY0, 6XNN, ANNN
+  TokenClear,    // 00E0
+  TokenRet,      // 00EE
+  TokenB,        // 1NNN
+  TokenCall,     // 2NNN
+  TokenIfe,      // 5XY0
+  TokenAdd,      // 7XNN, 8XY4, FX1E
+  TokenOr,       // 8XY1
+  TokenAnd,      // 8XY2
+  TokenXor,      // 8XY3
+  TokenSub,      // 8XY5
+  TokenLls,      // 8XY6
+  TokenSus,      // 8XY7
+  TokenLrs,      // 8XYE
+  TokenIfne,     // 9XY0
+  TokenBa,       // BNNN
+  TokenRand,     // CXNN
+  TokenDrw,      // DXYN
+  TokenIke,      // EX9E
+  TokenIkn,      // EXA1
+  TokenGdl,      // FX07
+  TokenGtk,      // FX0A
+  TokenSdl,      // FX15
+  TokenSsd,      // FX18
+  TokenSpr,      // FX29
+  TokenBcd,      // FX33
+  TokenDpr,      // FX55
+  TokenLdr,      // FX65
+  TokenReg,
+  TokenConst,
+  TokenAddr,
+  TokenComment
+} TokenType;
+
+typedef struct {
+  TokenType type;
+  char *data;
+} token_t;
+
+union opcode {
   struct __attribute__((packed)){
     uint16_t end: 4;
     uint16_t y: 4;
