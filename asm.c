24c15fd4db6ab6868613155872ab8f18fc2db8d4
Kode-Kun <lui.cardoso.1@ulaval.ca>
2026-03-26 11:34:25 -0400

    started work on symbol table and added register I handling

--- asm.c
+++ asm.c
@@ -6,7 +6,59 @@
 
 #include "common.h"
 
-instruction_t lex(char *line)
+struct symb_t {
+  char **symbols;
+  size_t size;
+} symbol_table;
+
+void init_symb(void)
+{
+  symbol_table.symbols = malloc(sizeof(char**));
+  symbol_table.size = 0;
+}
+
+void append_symb(char* symb)
+{
+  size_t symb_len = strlen(symb) + 1;
+  symbol_table.symbols[symbol_table.size] = malloc(symb_len);
+  strlcpy(symbol_table.symbols[symbol_table.size],
+	  symb, symb_len);
+  symbol_table.size++;
+}
+
+char *get_symb(char *query)
+{
+  char *sep = ":\n\t\r";
+  for(size_t i = 0; i < symbol_table.size; i++){
+    // get copy of symbol[i] in symbol table
+    int len = strlen(symbol_table.symbols[i]) + 1;
+    char *entry = malloc(len);
+    strlcpy(entry, symbol_table.symbols[i], len);
+
+    char *symbol = strtok(entry, sep);
+    if(strcmp(symbol, query) == 0) return strtok(NULL, sep);
+
+    free(entry);
+  }
+  return NULL;
+}
+
+uint16_t str_to_hex(char *str)
+{
+  uint16_t hex;
+  char *endptr;
+  long int decimal = strtol(str, &endptr, 0);
+
+  // bounds-checking: constants and addresses can't go above 0xFFF
+  if(decimal > 0xFFF){
+    str = NULL;
+  }
+
+  hex = (uint16_t) decimal;
+  return hex;
+}
+
+instruction_t lex(char *line, int linenum)
 {
   char *l = line;
   char *data;
@@ -23,6 +75,7 @@ instruction_t lex(char *line)
     l = NULL;
     token_t tok = nulltok;
 
+    // check for non-instruction tokens
     switch(data[0]){
     case ';':
       tok.type = TokenComment;
@@ -45,12 +98,17 @@ instruction_t lex(char *line)
       tok.type = TokenReg;
       tok.data = data;
       break;
+    case 'I':
+      tok.type = TokenReg;
+      tok.data = data;
+      break;
     case '#':
       tok.type = TokenConst;
       tok.data = data;
       break;
     case '0':
-      if(data[1] != 'x' && data[1] != 'X'){
+      if(data[1] != 'x' && data[1] != 'X' &&
+	 data[1] != 'b' && data[1] != 'B'){
 	tok.type = TokenNull;
 	tok.data = ADDRESS_NOTATION_ERROR;
 	break;
@@ -62,6 +120,7 @@ instruction_t lex(char *line)
       break;
     }
 
+    // check for instruction tokens
     if(tok.type == TokenNull && tok.data == NULL){
       for(int i = 0; i < ARRAY_SIZE(instructions); i++){
 	tok = nulltok;
@@ -72,6 +131,12 @@ instruction_t lex(char *line)
 	}
       }
     }
+
+    // if we still didn't get a match, return unkown error
+    if(tok.type == TokenNull && tok.data == NULL){
+      tok.data = UNKNOWN_ERROR;
+    }
+
     inst.tokens[inst.count++] = tok;
   }
   return inst;
@@ -136,20 +201,33 @@ int main(int argc, char **argv)
   char *line = NULL;
   size_t linelen;
   size_t linecap = 0;
-  //int linenum = 0;
+  int linenum = 1;
+  init_symb();
 
-  int j = 1;
   while((linelen = getline(&line, &linecap, input_f) != -1)){
-    instruction_t inst = lex(line);
-    fprintf(stderr, "Instruction %d\n", j);
+    instruction_t inst = lex(line, linenum);
+    fprintf(stderr, "Instruction %d {\n", linenum);
     for(int i = 0; i < inst.count; i++){
-      fprintf(stderr, "Token %d: %s of type %s\n", i+1, inst.tokens[i].data, token_to_str(inst.tokens[i].type));
+      token_t tok = inst.tokens[i];
+
+      fprintf(stderr, "  Token %d: %s of type %s\n", i+1, tok.data,
+	      token_to_str(tok.type));
+      if(tok.type == TokenAddr || tok.type == TokenConst){
+	fprintf(stderr, "Value of %s: %d.\n", tok.data, tok.type == TokenConst ? str_to_hex(&tok.data[1]) : str_to_hex(tok.data));
+      }
     }
+    fprintf(stderr, "}\n");
     //opcode op = parse(lex(line)); // we parse the instruction returned by lex()
     //if(op.raw == 0x0000) continue;
     //write_be16(rom_f, op.raw)t
-    j++;
+    linenum++;
   }
+
+  append_symb(".label1:0x223");
+  append_symb(".label2:0x069");
+
+  printf("%s\n", get_symb(".label1"));
+  printf("%s\n", get_symb(".label2"));
 
   printf("Input argument: %s\n", input_path);
   printf("Rom path: %s\n", rom_path);
